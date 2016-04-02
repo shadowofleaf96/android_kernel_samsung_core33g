f0678b11d10e639b7e2ae9b87927debf3d5fa292
nikit002 <nikitsharma92@gmail.com>
2016-04-02 20:45:29 +0530

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 SMP PREEMPT Sat Apr 2 11:37:25 IST 2016"
+#define LINUX_COMPILE_BY "vegito"
+#define LINUX_COMPILE_HOST "vegito-Veriton-Series"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
